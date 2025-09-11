75c78275b6acc104d344371eb5382fd5a7ee1a74
enlightened1one <nemiwebdev@gmail.com>
2025-09-11 15:01:45 +0100

    i ran  npm i motion clsx tailwind-merge to set-up aceternity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
    <>
     <main>
       <div>
-        <h1>Welcome to Alpha Real Estate</h1>
-        <p>Your trusted partner in finding your dream home.</p>
+        <h1 className="text-4xl font-bold" >Welcome to Alpha Real Estate</h1>
+        <p className="text-lg" >Your trusted partner in finding your dream home.</p>
       </div>
     </main>
    </>
