913b5e22a37a73d03243cf2122ba45f9812bc061
enlightened1one <nemiwebdev@gmail.com>
2025-09-12 11:25:16 +0100

    geist font is now working and i can now do same with any other font i want to use. also modified the layout of hero.tsx so the head text is well placed, leaving some space for the right hand side of the hero

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,19 +1,38 @@
 "use client";
-import React from "react";
+// import React from "react";
 import { WavyBackground } from "./ui/wavy-background";
+import "./Hero.scss"
+import localFont from "next/font/local";
+// import "../../../node_modules/geist/dist/fonts/geist-sans/Geist-Variable.woff2";
+
+// Define font at the module scope
+const geistSans = localFont({
+  src: "../../../node_modules/geist/dist/fonts/geist-sans/Geist-Variable.woff2",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+  preload: true,
+});
 
 export function Hero() {
   return (
-    <WavyBackground className="border-2 border-red-500  w-full h-full">
-    <div className="pb-40 bg flex flex-col justify-center items-center w-full h-full">
-      <p className={`text-xl text-nowrap md:text-4xl lg:text-7xl text-white font-bold font-[Geist Sans] geist-sans text-center`}>
-        Welcome to Alpha Real Estate
+    // dark:bg-[#171717]
+    <main className="hero-1 flex flex-row justify-stretch items-stretch w-full h-screen p-0  bg-[#0a0a0a] px-4" >
+    <div className="flex flex-row w-full h-full justify-between items-stretch">
+      <div className="flex flex-col justify-end items-start h-full w-full min-h-[100%] py-5">
+          <p className={`text-xl wrap-normal text-left md:text-4xl lg:text-7xl text-white font-bold ${geistSans.className}`}>
+        T.C.S Properties
       </p>
-      <p className={`text-base md:text-lg mt-4 text-white font-normal inter-var text-center`}>
+      <p className={`text-base md:text-lg  text-left mt-4 text-white font-normal ${geistSans.className}`}>
         Your trusted partner in finding your dream home.
-      </p>      
-    </div>
+      </p> 
+      </div>
+      <div className="border-2 w-full h-full flex-col justify-center items-center">
 
-    </WavyBackground>
+      </div>
+    </div>
+     
+    </main>
+
+ 
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,66 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+ 
+const geist = Geist({
+  subsets: ['latin'],
+})
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className={geist.className}>
+      <body>{children}</body>
+    </html>
+  )
+}
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Alpha Real Estate",  //This title will appear in the browser tab
   description: "Find your dream home with Alpha Real Estate - Your trusted partner in real estate.", //This description is used by search engines
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
-}
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <body
+//         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+//       >
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
